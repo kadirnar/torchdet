7383ea06a2753cce1a2c401ed1b9aba8c07a791f
Kadir Nar <kadir.nar@hotmail.com>
2022-04-13 20:55:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import numpy as np
 
 
 model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-model.load_state_dict(torch.load('models/maskrcnn_resnet50_fpn_coco.pth'))
+# model.load_state_dict(torch.load('models/maskrcnn_resnet50_fpn_coco.pth'))
 model.eval()
 
-img = "images/1.jpg"
+img = "1.jpg"
 
 # tensor img
 tensor_img = torch.Tensor(cv2.imread(img))
@@ -35,22 +35,14 @@ masks = predictions[0]['masks'].detach().numpy()
 # threshold
 threshold = 0.5
 
-predictions_scores, predictions_boxes, predictions_labels, predictions_masks = [], [], [], []
 
+prediction_list = []
 for thd in range(0, len(scores)):
     if scores[thd] > threshold:
-        predictions_scores.append(scores[thd])
-        predictions_boxes.append(boxes[thd])
-        predictions_labels.append(labels[thd])
-        predictions_masks.append(masks[thd])
+        prediction_list.append({'score': scores[thd], 'box': boxes[thd], 'label': labels[thd], 'mask': masks[thd]})
+  
 
-for i, box in enumerate(predictions_boxes):
-    score = predictions_scores[i]
-    labels = '%{} {}'.format(float(int(score * 100)), predictions_labels[i])
+for i, box in enumerate(prediction_list[0]['box']):
+    score = prediction_list[0]["score"][i]
+    labels = '%{} {}'.format(float(int(score * 100)), prediction_list[0]["label"][i])
     x, y, w, h = box[0], box[1], box[2], box[3]
-    # cv2 sürüm hatası çözülmesi gerekiyor.
-    cv2.rectangle(numpy_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.putText(numpy_img, labels, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.imshow('img', numpy_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
